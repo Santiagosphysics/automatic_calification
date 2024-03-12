a60be2fe63e4f826d5f72eb1a4bcec167d9b830f
Santiagosphysics <msjimenez@unbosque.edu.co>
2024-03-11 21:25:21 -0500

    1.0.4

--- reader.py
+++ reader.py
@@ -6,3 +6,23 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 import logging 
+
+logger = tf.get_logger()
+
+logger.setlevel(logging.ERROR)
+
+dataset, metadata = tfds.load('mnist', as_suppervised = True, with_info = True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+class_names = [
+    'Cero', 'Uno', 'Dos','tres','Cuatro','Cinco','Seis','Siete','Ocho','Nueve'
+]
+
+num_train_examples = metadata.splits['train'].num_examples 
+num_test_examples = metadata.splits['test'].num_examples 
+
+# Normalizar, de cero a uno 
+def normalize
+
+
+çç
