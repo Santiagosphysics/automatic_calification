6581cfe738bc92905e700f9e887a088adab7642e
Santiagospysics <msjimenez@unbosque.edu.co>
2024-03-12 12:46:46 -0500

    1.0.5

--- reader.py
+++ reader.py
@@ -22,7 +22,97 @@ num_train_examples = metadata.splits['train'].num_examples
 num_test_examples = metadata.splits['test'].num_examples 
 
 # Normalizar, de cero a uno 
-def normalize
+def normalize(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
+
+train_dataset = train_dataset.map(normalize)
+test_dataset = test_dataset.map(normalize)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28,28,1)),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax) #para clasificación
+])
 
 
-çç
+model.compile(optimizer='adam',loss='sparse_ategorical_crossentropy', metrics=['accuracy'])
+
+#Aprendizaje por lotes de 32 cada lote
+
+
+BATCHSIZE = 32 
+train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCHSIZE)
+test_dataset = test_dataset.batc(BATCHSIZE)
+
+model.fit(train_dataset, epocs = 5, steps_per_epoch =math.ceil(num_train_examples/BATCHSIZE))
+
+test_loss, test_accuracy = model.evaluate(
+    test_dataset, steps=math.ceil(num_test_examples/BATCHSIZE)
+)
+
+print('Results of the test: ', test_accuracy)
+
+for test_images, test_labels in test_dataset.take(1):
+    test_images = test_images.numpy()
+    test_labels = test_labels.numpy()
+    predictions = model.predict(test_images)
+
+
+
+def plot_image(i, predictions_array, true_labels, images):
+	predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+	plt.grid(False)
+	plt.xticks([])
+	plt.yticks([])
+
+	plt.imshow(img[...,0], cmap=plt.cm.binary)
+
+	predicted_label = np.argmax(predictions_array)
+	if predicted_label == true_label:
+		color = 'blue'
+	else:
+		color = 'red'
+
+	plt.xlabel("Prediccion: {}".format(class_names[predicted_label]), color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+	predictions_array, true_label = predictions_array[i], true_label[i]
+	plt.grid(False)
+	plt.xticks([])
+	plt.yticks([])
+	thisplot = plt.bar(range(10), predictions_array, color="#888888")
+	plt.ylim([0,1])
+	predicted_label = np.argmax(predictions_array)
+
+	thisplot[predicted_label].set_color('red')
+	thisplot[true_label].set_color('blue')
+
+numrows=5
+numcols=3
+numimages = numrows*numcols
+
+plt.figure(figsize=(2*2*numcols, 2*numrows))
+for i in range(numimages):
+	plt.subplot(numrows, 2*numcols, 2*i+1)
+	plot_image(i, predictions, test_labels, test_images)
+	plt.subplot(numrows, 2*numcols, 2*i+2)
+	plot_value_array(i, predictions, test_labels)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
