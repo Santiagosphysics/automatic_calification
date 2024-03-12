b346a79ea684bdf8b94086a7dafd2f1392dd3caa
Santiagosphysics <msjimenez@unbosque.edu.co>
2024-03-11 21:15:52 -0500

    1.0.3

---
+++ reader.py
@@ -0,0 +1,8 @@
+# from __feature__ import absolute_import, fivision, print_function, unicode_literals
+import tensorflow as tf 
+import tensorflow_datasets as tfds 
+
+import math 
+import numpy as np 
+import matplotlib.pyplot as plt
+import logging 
